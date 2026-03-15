2ba829cad5e1979fcfce06b1de4d4750cb4637ba
Minho <rkdalsgh0106@naver.com>
2026-03-15 23:20:17 +0900

    feat: 종료하기 버튼 클릭시 홈으로 이동

--- apps/client/src/features/exam/components/header/ExamHeader.tsx
+++ apps/client/src/features/exam/components/header/ExamHeader.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 import { ExitLine } from "@/shared/icons";
 
 export const ExamHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="ml-auto">
-      <button className="bg-gs6 flex h-11 items-center justify-center gap-x-1.5 rounded-xl px-4 py-3">
+      <button
+        className="bg-gs6 flex h-11 items-center justify-center gap-x-1.5 rounded-xl px-4 py-3"
+        onClick={() => navigate("/")}
+      >
         <span className="text-gs1 text-[17px] font-bold">종료하기</span>
         <ExitLine />
       </button>
